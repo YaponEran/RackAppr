6845e2d1532f488dbe16fd46063e742013f75d9d
YaponEran <erannodepp9.4@gmail.com>
2021-03-22 10:09:53 +0600

    app is working

--- app.rb
+++ app.rb
@@ -1,9 +1,48 @@
-require_relative 'formatr'
+require_relative 'formatter'
 
 class App
 
-  def initialize()
-    
+  def call(env)
+    @request = Rack::Request.new(env)
+    response
+    [status, headers, body]
+  end
+
+  private
+  def response
+    return wrong_path unless @request.path_info == '/time'
+    @formatter = Formatter.new(@request.params)
+    @formatter.check_format
+    return unknown_format unless @formatter.success?
+
+    fortmated_time
+  end
+
+  def status
+    @status_code
+  end
+
+  def headers
+    { "Content-Type" => "text/plain" }
+  end
+
+  def body
+    ["#{@message}"]
+  end
+
+  def fortmated_time
+    @status_code = 200
+    @message = @formatter.time
+  end
+
+  def wrong_path
+    @status_code = 404
+    @message = "Page not found"
+  end
+
+  def unknown_format
+    @status_code = 400
+    @message = "Unknown format"
   end
 
 end
---
+++ formatter.rb
@@ -0,0 +1,32 @@
+class Formatter
+
+  TIME_FORMATS = {
+    "year" => "%Y",
+    "month" => "%m",
+    "day" => "%d",
+    "hour" => "%Hh",
+    "minute" => "%Mm",
+    "second" => "%Ss"
+  }.freeze
+
+  attr_reader :incorrect
+
+  def initialize(params)
+    @params = params['format'].split(',')
+  end
+
+  def check_format
+    @correct, @incorrect = @params.partition { |format| TIME_FORMATS[format] }
+  end
+
+  def success?
+    @incorrect.empty?
+  end
+
+  def time
+    formats = @correct.map { |format| TIME_FORMATS[format] }
+    Time.now.strftime(formats.join('-'))
+  end
+
+end
+
